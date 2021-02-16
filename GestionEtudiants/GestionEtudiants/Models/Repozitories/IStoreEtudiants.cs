7e3a02e3b87c3bcef9ce03ed89a30925bb4f14f1
jihad <jihad.karamane91@gmail.com>
2021-02-16 15:50:23 +0100

    Commit-1

---
+++ GestionEtudiants/GestionEtudiants/Models/Repozitories/IStoreEtudiants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionEtudiants.Models.Repozitories
+{
+    public interface IStoreEtudiants<TEntity>
+    {
+        IList<TEntity> List();
+
+        public TEntity Rechercher(int id);
+        void Ajouter(TEntity entity);
+        void Modifier(int id, TEntity entity);
+        void Suprimer(int id);
+        void Add(Etudiants etudiants);
+        object List(int id);
+    }
+}
