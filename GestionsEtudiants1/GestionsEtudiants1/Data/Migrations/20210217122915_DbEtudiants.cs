7f23839ac58ed0da16f22ca162b45c93ff38b172
jihad <jihad.karamane91@gmail.com>
2021-02-17 13:38:03 +0100

    G.Etudiants

---
+++ GestionsEtudiants1/GestionsEtudiants1/Data/Migrations/20210217122915_DbEtudiants.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestionsEtudiants1.Data.Migrations
+{
+    public partial class DbEtudiants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Etudiant",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nom = table.Column<string>(nullable: true),
+                    prénom = table.Column<string>(nullable: true),
+                    CIN = table.Column<string>(nullable: true),
+                    Adresse = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Etudiant", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Etudiant");
+        }
+    }
+}
---
+++ GestionsEtudiants1/GestionsEtudiants1/Models/Etudiant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionsEtudiants1.Models
+{
+    public class Etudiant
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+        public string prénom { get; set; }
+        public string CIN { get; set; }
+        public string Adresse { get; set; }
+
+        public Etudiant() {
+
+        }
+    }
+}
